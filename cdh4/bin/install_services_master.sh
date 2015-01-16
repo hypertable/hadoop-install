3e82f6333aba64146d0fd5c3bcabc7316f5ca284
Doug Judd <doug@hypertable.com>
2015-01-16 13:16:03 -0800

    Fixed creation of mapred.local.dir directories (CDH4)

--- cdh4/bin/install_services_master.sh
+++ cdh4/bin/install_services_master.sh
@@ -5,10 +5,16 @@ if [ $# -eq 0 ] ; then
     exit 0
 fi
 
+MAPRED_DIRS=
 NAMENODE_DIRS=
 for arg in "$@"; do
+    MAPRED_DIRS="${MAPRED_DIRS} ${arg}/mapred/local"
     NAMENODE_DIRS="${NAMENODE_DIRS} ${arg}/dfs/nn"
 done
+
+mkdir -p ${MAPRED_DIRS}
+chmod 755 ${MAPRED_DIRS}
+chown -R mapred:hadoop ${MAPRED_DIRS}
 
 mkdir -p ${NAMENODE_DIRS}
 chmod 700 ${NAMENODE_DIRS}

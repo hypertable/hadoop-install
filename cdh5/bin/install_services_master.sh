d5d386560e9dfefefecad731570b9058c15734ab
Doug Judd <doug@hypertable.com>
2015-06-03 08:58:32 -0700

    Fixed creation of mapred.local.dir directories (CDH5)

--- cdh5/bin/install_services_master.sh
+++ cdh5/bin/install_services_master.sh
@@ -7,10 +7,16 @@ fi
 
 yum -y install hadoop-yarn-resourcemanager hadoop-hdfs-namenode
 
+MAPRED_DIRS=
 NAMENODE_DIRS=
 for arg in "$@"; do
+    MAPRED_DIRS="${MAPRED_DIRS} ${arg}/mapred/local"
     NAMENODE_DIRS="${NAMENODE_DIRS} ${arg}/dfs/nn"
 done
+
+mkdir -p ${MAPRED_DIRS}
+chmod 755 ${MAPRED_DIRS}
+chown -R mapred:hadoop ${MAPRED_DIRS}
 
 mkdir -p ${NAMENODE_DIRS}
 chmod 700 ${NAMENODE_DIRS}
